6c3cbbfb51e1531bb2dd4dac6877729f540077c9
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-07-22 09:30:00 -0400

    added changes to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,10 @@
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SearchBooks from "./pages/SearchBooks";
